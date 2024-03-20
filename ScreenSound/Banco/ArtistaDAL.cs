312a7a98077abb057987e2d56ee0844bcd84e9e7
User <User@DESKTOP-5C2E3UU>
2024-03-20 12:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ScreenSound.Banco
+{
+    internal class ArtistaDAL : DAL<Artista>
+    {
+        public ArtistaDAL(ScreenSoundContext context) : base(context) { }
+
+        public Artista? RecuperarPeloNome(string nome)
+        {
+            return context.Artistas.FirstOrDefault(a => a.Nome.Equals(nome));
+        }
+    }
+
+}
---
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class MusicaDAL : DAL<Musica>
+    {
+        public MusicaDAL(ScreenSoundContext context): base(context) { }
+        
+        public Musica? RecuperarPeloNome(string nome)
+        {
+            return context.Musicas.FirstOrDefault(a => a.Nome.Equals(nome));
+        }
+    }
+}
